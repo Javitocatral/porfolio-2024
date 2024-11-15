43b4cbae4c8d08d26fdc5c59e1ec27915d4ab712
Javier Gascon <javitocatral@gmail.com>
2024-11-15 12:11:01 +0100

    cv

--- src/Porfolio.jsx
+++ src/Porfolio.jsx
@@ -18,15 +18,16 @@ function Porfolio() {
             actitud y adaptabilidad. Siempre actualizando mi formación, acabo de
             finalizar mi formación en <strong>Ironhack </strong> Ironhack, donde
             profundicé en tecnologías como <strong>JavaScript</strong>,{' '}
-            <strong>Node.js</strong> y <strong>MongoDB</strong>.
+            <strong>Node.js, Express</strong> y <strong>MongoDB</strong>.
           </p>
           <p>
             Me encanta trabajar con <strong>React.js</strong> y{' '}
             <strong>SvelteKit</strong> para crear interfaces de usuario
-            dinámicas, y tengo una sólida base en HTML y CSS para conseguir
-            páginas más atractivas y fáciles de usar. También disfruto
-            asegurándome de que el código esté limpio y bien organizado, usando
-            herramientas como <strong> ESLint, Prettier y Playwright</strong>.
+            dinámicas, y tengo una sólida base en<strong> HTML y CSS</strong>{' '}
+            para conseguir páginas más atractivas y fáciles de usar. También
+            disfruto asegurándome de que el código esté limpio y bien
+            organizado, usando herramientas como{' '}
+            <strong> ESLint, Prettier y Playwright</strong>.
           </p>
           <p>
             Tengo muchas ganas de seguir creciendo profesionalmente y de aportar
